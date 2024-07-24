d203e27b158442f0e78ac69ab27bece40bd38ca3
Artem <nikolloteslla@gmail.com>
2024-07-24 13:30:22 +0300

    add desktop

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,30 @@
-$(function () {
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('header');
+  if (window.scrollY > 0) {
+      header.classList.add('sticky');
+  } else {
+      header.classList.remove('sticky');
+  }
+});
 
-})
+
+
+var swiper = new Swiper(".team__slider", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".review__slider", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
