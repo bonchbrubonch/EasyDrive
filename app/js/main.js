7899d2594c19cc39eb3c12e1116c8a9de740e69e
Artem <nikolloteslla@gmail.com>
2024-07-24 14:43:25 +0300

    add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,37 @@
-window.addEventListener('scroll', function() {
+window.addEventListener('scroll', function () {
   var header = document.querySelector('header');
   if (window.scrollY > 0) {
-      header.classList.add('sticky');
+    header.classList.add('sticky');
   } else {
-      header.classList.remove('sticky');
+    header.classList.remove('sticky');
   }
 });
 
+document.querySelector(".header__menu-btn").addEventListener("click", function () {
+  this.classList.toggle("active");
+  document.querySelector(".header__nav").classList.toggle("open");
+  document.body.classList.toggle("lock");
+  document.querySelector("header").classList.toggle("active"); // Add this line
+});
 
 
 var swiper = new Swiper(".team__slider", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      spaceBetween: 30,
+      slidesPerView: 2,
+    },
+    992: {
+      spaceBetween: 30,
+      slidesPerView: 3,
+    },
   },
 });
 
